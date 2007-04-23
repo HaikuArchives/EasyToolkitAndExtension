b664d2cd66938d2291380301c754af66866e70c8
anthonylee <anthonylee@7b05ed68-8f2e-0410-9b1b-f431d2f17f4f>
2007-04-23 04:17:17 +0000

    Added another helloworld.
    git-svn-id: http://svn.osdrawer.net/etoolkit@3 7b05ed68-8f2e-0410-9b1b-f431d2f17f4f

--- etkxx-sample/interface/helloworld2.cpp
+++ etkxx-sample/interface/helloworld2.cpp
@@ -29,85 +29,39 @@
 
 #include <etkxx.h>
 
-class TApplication : public EApplication {
-public:
-	TApplication();
-};
+#define MSG_HELLO_WORLD		'helo'
 
 
-class TWindow : public EWindow {
-public:
-	TWindow(ERect frame, const char* title, e_window_type type, euint32 flag);
-	virtual bool QuitRequested();
-};
-
-
-class TView : public EView {
-public:
-	TView(ERect frame, const char *name, euint32 resizingMode, euint32 flag);
-	virtual void Draw(ERect updateRect);
-};
-
-
-TApplication::TApplication()
-	: EApplication("application/x-vnd.etkxx-helloworld2-app")
-{
-	TWindow *t_win = new TWindow(ERect(100, 100, 300, 150), "Hello World Sample 2", E_DOCUMENT_WINDOW, 0);
-
-#if 0
-	Lock();
-	t_win->Lock();
-	t_win->ProxyBy(this);
-	t_win->Unlock();
-	Unlock();
-#endif
-
-	t_win->Lock();
-	t_win->Show();
-	t_win->Unlock();
-}
-
-
-TWindow::TWindow(ERect frame, const char* title, e_window_type type, euint32 flag)
-	: EWindow(frame, title, type, flag)
-{
-	frame.OffsetTo(0, 0);
-
-	AddChild(new TView(frame, "theView", E_FOLLOW_NONE, E_WILL_DRAW));
-}
-
-
-bool TWindow::QuitRequested()
+static e_filter_result hello_filter_hook(EMessage *message, EHandler **target, EMessageFilter *filter)
 {
 	etk_app->PostMessage(E_QUIT_REQUESTED);
-	return true;
-}
-
-
-TView::TView(ERect frame, const char* title, euint32 resizingMode, euint32 flags)
-	: EView(frame, title, resizingMode, flags)
-{
-	SetViewColor(200, 200, 200);
-
-	EFont font;
-	GetFont(&font);
-	font.SetSize(20);
-
-	SetFont(&font, E_FONT_SIZE);
-}
-
-
-void TView::Draw(ERect updateRect)
-{
-	MovePenTo(20, 20);
-	DrawString("Hello World.");
+	return E_SKIP_MESSAGE;
 }
 
 
 int main(int argc, char **argv)
 {
-	TApplication app;
-	app.Run();
+	new EApplication("application/x-vnd.etkxx-helloworld2-app");
+
+	EWindow *win = new EWindow(ERect(100, 100, 300, 170),
+				   "Hello World Sample 2",
+				   E_TITLED_WINDOW, 0);
+	EButton *btn = new EButton(ERect(20, 20, 180, 50), NULL,
+				   "Hello World",
+				   new EMessage(MSG_HELLO_WORLD),
+				   E_FOLLOW_ALL);
+
+
+	win->Lock();
+	win->AddChild(btn);
+	win->Show();
+	win->AddCommonFilter(new EMessageFilter(MSG_HELLO_WORLD, hello_filter_hook));
+	win->Unlock();
+
+	etk_app->Run();
+
+	delete etk_app;
+
 	return 0;
 }
 
--- etkxx-sample/interface/helloworld3.cpp
+++ etkxx-sample/interface/helloworld3.cpp
@@ -23,36 +23,91 @@
  * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
  * IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
- * File: helloworld1.cpp
+ * File: helloworld3.cpp
  *
  * --------------------------------------------------------------------------*/
 
 #include <etkxx.h>
 
+class TApplication : public EApplication {
+public:
+	TApplication();
+};
+
+
+class TWindow : public EWindow {
+public:
+	TWindow(ERect frame, const char* title, e_window_type type, euint32 flag);
+	virtual bool QuitRequested();
+};
+
+
+class TView : public EView {
+public:
+	TView(ERect frame, const char *name, euint32 resizingMode, euint32 flag);
+	virtual void Draw(ERect updateRect);
+};
+
+
+TApplication::TApplication()
+	: EApplication("application/x-vnd.etkxx-helloworld3-app")
+{
+	TWindow *t_win = new TWindow(ERect(100, 100, 300, 150), "Hello World Sample 3", E_DOCUMENT_WINDOW, 0);
+
+#if 0
+	Lock();
+	t_win->Lock();
+	t_win->ProxyBy(this);
+	t_win->Unlock();
+	Unlock();
+#endif
+
+	t_win->Lock();
+	t_win->Show();
+	t_win->Unlock();
+}
+
+
+TWindow::TWindow(ERect frame, const char* title, e_window_type type, euint32 flag)
+	: EWindow(frame, title, type, flag)
+{
+	frame.OffsetTo(0, 0);
+
+	AddChild(new TView(frame, "theView", E_FOLLOW_NONE, E_WILL_DRAW));
+}
+
+
+bool TWindow::QuitRequested()
+{
+	etk_app->PostMessage(E_QUIT_REQUESTED);
+	return true;
+}
+
+
+TView::TView(ERect frame, const char* title, euint32 resizingMode, euint32 flags)
+	: EView(frame, title, resizingMode, flags)
+{
+	SetViewColor(200, 200, 200);
+
+	EFont font;
+	GetFont(&font);
+	font.SetSize(20);
+
+	SetFont(&font, E_FONT_SIZE);
+}
+
+
+void TView::Draw(ERect updateRect)
+{
+	MovePenTo(20, 20);
+	DrawString("Hello World.");
+}
+
+
 int main(int argc, char **argv)
 {
-	new EApplication("application/x-vnd.etkxx-helloworld1-app");
-
-	EWindow *win = new EWindow(ERect(100, 100, 300, 170),
-				   "Hello World Sample 1",
-				   E_TITLED_WINDOW,
-				   E_QUIT_ON_WINDOW_CLOSE);
-	EButton *btn = new EButton(ERect(20, 20, 180, 50), NULL,
-				   "Hello World",
-				   new EMessage(E_QUIT_REQUESTED),
-				   E_FOLLOW_ALL);
-
-	btn->SetTarget(etk_app);
-
-	win->Lock();
-	win->AddChild(btn);
-	win->Show();
-	win->Unlock();
-
-	etk_app->Run();
-
-	delete etk_app;
-
+	TApplication app;
+	app.Run();
 	return 0;
 }
 
