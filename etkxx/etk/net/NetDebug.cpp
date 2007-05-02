d8cf2167b021c29822fd65f93e2025416c473dc4
anthonylee <anthonylee@7b05ed68-8f2e-0410-9b1b-f431d2f17f4f>
2007-05-02 19:41:59 +0000

    Corrected ENetDebug::Dump()
    git-svn-id: http://svn.osdrawer.net/etoolkit@27 7b05ed68-8f2e-0410-9b1b-f431d2f17f4f

--- etkxx/etk/net/NetDebug.cpp
+++ etkxx/etk/net/NetDebug.cpp
@@ -104,7 +104,7 @@ ENetDebug::Dump(const char *data, size_t len, const char *title)
 	{
 		for(int i = 0; i < 16 && len > 0; i++, len--, data++)
 		{
-			fprintf(stderr, "%x", *data);
+			fprintf(stderr, "%02x", *data);
 			if(!(i == 15 || len == 1)) fputc(' ', stderr);
 		}
 		fputs("\n", stderr);
